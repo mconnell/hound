149e538656f24833776c336f16f6f4cc65dfec9d
Mark Connell <mark@origami.co.uk>
2009-11-14 23:34:49 +0000

    Remove the validation of an account needing to be present on a user. Altering the plugin to use a before_validation solves this being an issue for implementation but changing the callback makes is leaving me a bit stumped on getting FactoryGirl to do the right thing as I can't inject Account.current.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,4 @@
 class User < ActiveRecord::Base
   acts_as_authentic
   acts_as_restricted_subdomain :through => :account
-
-  # validations
-  validates_presence_of :account
-
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,12 +4,4 @@ describe User do
   it "should generate a valid model from the Factory" do
     Factory.build(:user).should be_valid
   end
-
-  describe "validations" do
-    it "should only be valid if associated to an account" do
-      user = Factory.build(:user, :account => nil)
-      user.should_not be_valid
-      user.should have(1).error_on(:account)
-    end
-  end
 end
