f434adbe6a0e1c8324d23a88b3d3b0eec5596d15
Mark Connell <mark@origami.co.uk>
2009-11-14 00:12:01 +0000

    fix the prefab authentication spec helpers'

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -7,16 +7,16 @@ def current_user(stubs = {})
   @current_user ||= mock_model(User, stubs)
 end
 
-def user_session(stubs = {}, user_stubs = {})
+def current_user_session(stubs = {}, user_stubs = {})
   @current_user_session ||= mock_model(UserSession, {:user => current_user(user_stubs)}.merge(stubs))
 end
 
 def login(session_stubs = {}, user_stubs = {})
-  UserSession.stub!(:find).and_return(user_session(session_stubs, user_stubs))
+  UserSession.stub!(:find).and_return(current_user_session(session_stubs, user_stubs))
 end
 
 def logout
-  @user_session = nil
+  @current_user_session = nil
 end
 
 Spec::Runner.configure do |config|
