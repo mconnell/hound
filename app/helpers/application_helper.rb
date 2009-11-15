407e355f8bbb32cca90b72a5f435b6d4754945ac
Mark Connell <mark@origami.co.uk>
2009-11-15 22:54:23 +0000

    Add page_heading helper method.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,13 @@
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
+
+  # inserts a page heading into the layout file
+  # use this instead of a <h1> tag
+  # eg. - page_heading "foo #{bar}"
+  def page_heading(text)
+    content_for :page_heading do
+      h(text)
+    end
+  end
+
 end
