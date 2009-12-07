bcb0ed1a2e80fcc08d99e50a3562ca9ce3dd1141
Mark Connell <mark@origami.co.uk>
2009-12-07 00:30:10 +0000

    Basic Events

--- app/models/domain_observer.rb
+++ app/models/domain_observer.rb
@@ -1,5 +1,6 @@
 class DomainObserver < ActiveRecord::Observer
   def after_build_profile(domain, transition)
+    Event.create(:object => domain, :object_action => 'build_profile')
     domain.send_later(:refresh)
   end
 end
