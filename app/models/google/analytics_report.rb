c6d617e4d7dfdc2fb4f6761e707d402f37bc7791
Mark Connell <mark@origami.co.uk>
2010-01-01 15:16:00 +0000

    Basic method to fetch google analytics data.

--- app/models/google/analytics_report.rb
+++ app/models/google/analytics_report.rb
@@ -1,6 +1,36 @@
+# Model used for capturing Google analytics data
+# fetch_report_data is called in a before_create filter
+# allowing the start_at and end_at dates to be modified
+# before submitting a request to Google for data.
+# eg.
+# @domain.google_analytics_reports.create!(:start_at => 1.month.ago.beginning_of_day)
+# will set the start_at prior to the report request being submitted.
 class Google::AnalyticsReport < ActiveRecord::Base
   set_table_name "google_analytics_reports"
 
   belongs_to :domain
+
+  before_create :fetch_report_data
+
+  private
+  def fetch_report_data
+    start_date = start_at || 1.day.ago.beginning_of_day
+    end_date   = end_at   || 1.day.ago.end_of_day
+
+    if domain.present? && domain.ga_tracking_code.present?
+
+      Garb::Session.login('msn@markconnell.co.uk', 'password123')
+      report = Garb::Report.new(Garb::Profile.first(domain.ga_tracking_code),
+                                :start_date => start_date,
+                                :end_date   => end_date)
+      report.metrics :visits, :page_views
+      result = report.results[0]
+
+      self.start_at   = start_date
+      self.end_at     = end_date
+      self.visits     = result.visits
+      self.page_views = result.pageviews
+    end
+  end
 
 end
