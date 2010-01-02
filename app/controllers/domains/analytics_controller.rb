e3f96b3425ed21625c7a9c06db2bd6c2d9286af2
Mark Connell <mark@origami.co.uk>
2010-01-02 19:37:22 +0000

    This months and last months analytics.

--- app/controllers/domains/analytics_controller.rb
+++ app/controllers/domains/analytics_controller.rb
@@ -4,6 +4,18 @@ class Domains::AnalyticsController < ApplicationController
   before_filter :find_domain, :set_active_navigation
 
   def index
+    @current_month_visits = returning [] do |values|
+      Domain.first.google_analytics_reports.current_month.each do |report|
+        values << report.visits
+      end
+    end
+
+    @last_month_visits = returning [] do |values|
+      Domain.first.google_analytics_reports.last_month.each do |report|
+        values << report.visits
+      end
+    end
+
     respond_to do |format|
       format.html
     end
